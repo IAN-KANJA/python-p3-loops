66a7c577d4bb19d4544fcf5f2a628b0f3e24afbc
iannjau <lantanacreations@gmail.com>
2024-05-22 10:37:40 +0300

    add

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter in range(10, 0, -1):
+        print(happy_new_year)
+        counter -= 1
+     # type: ignore
+       # code goes here!
+
 
 def square_integers(int_list):
+   square_integers([1, 2, 3, 4, 5])
+   return (int_list)*2
     # code goes here!
-    pass
-
-def fizzbuzz():
-    # code goes here!
-    pass
+def fizzbuzz(num):
+    for num in range( 1 ,100):
+        if not num % 15:
+            print("FizzBuzz")
+        elif not num % 5:
+            print("Buzz")
+        elif not num % 3:
+            print("Fizz")
+        
+            
